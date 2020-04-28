e9a0a0a42c7294539c7ff490ef0283e4ef88ad9e
chepishvlad <vladchepish28@gmail.com>
2020-04-28 08:43:44 +0300

    first commit

---
+++ src/main/java/com/chepish/addr/AddrApplication.java
@@ -0,0 +1,15 @@
+package com.chepish.addr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddrApplication.class, args);
+	}
+
+
+
+}
